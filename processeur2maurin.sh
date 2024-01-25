e95c4fd9e88501353a2277bdcf9e621bea575852
the0phil3 <theophile.miailhe@gmail.com>
2024-01-25 16:18:14 +0100

    bash script improving

--- processeur2maurin.sh
+++ processeur2maurin.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
+# Constants
 INPUT='input/'
 OUTPUT='output/'
-LINE='------------------------------------------------------'
+LINE='--------------------------------------------------------------------------------------------------------'
+files=("models/kraken_htrtrained.mlmodel" "models/line_seg.mlmodel" "models/best.pt")
 
+# Check if all files are present
+for file in "${files[@]}"; do
+    if [ ! -e "$file" ]; then
+        echo "Error: File $file is not present in the directory."
+        exit 1  # Exit with an error code
+    fi
+done
+
+# Pre-running messages
+echo "All models are present in the directory."
 echo Here are all the files that the programme will run on :
 echo $INPUT*.jpg
 echo $LINE
 
-for file in $INPUT*.jpg; do
-	echo "$file"
-	yaltai kraken --device cpu -I "$file" --suffix ".xml" segment --yolo /Users/Theo/Desktop/seg_models/big_seg_model_v2/weights/best.pt -i line_seg.mlmodel
-	kraken -a -I "$file" -o _ocr.xml -f xml ocr -m kraken_htrtrained.mlmodel
-done
-#python csv_converter.py
+# Starts treatment
+cd $INPUT
+yaltai kraken --device cpu -I "*.jpg" --suffix ".xml" segment --yolo ../models/best.pt -i ../models/line_seg.mlmodel
+kraken -a -I '*.xml' -o _ocr.xml -f xml ocr -m ../models/kraken_htrtrained.mlmodel
+cd ..
+find $INPUT -type f -name "*ocr*" -exec mv {} $OUTPUT \;
+
